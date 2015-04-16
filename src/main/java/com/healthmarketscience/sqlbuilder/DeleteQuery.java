959f02520fd2a323356037c234219f904ca7244d
James Ahlborn <jtahlborn@yahoo.com>
2015-04-16 21:28:19 +0000

    relicense to apache license
    git-svn-id: https://svn.code.sf.net/p/openhms/code/sqlbuilder/trunk@447 f9ba57bc-9804-47c6-9de9-2f2ff8aac994

--- src/main/java/com/healthmarketscience/sqlbuilder/DeleteQuery.java
+++ src/main/java/com/healthmarketscience/sqlbuilder/DeleteQuery.java
@@ -1,28 +1,17 @@
 /*
 Copyright (c) 2008 Health Market Science, Inc.
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+    http://www.apache.org/licenses/LICENSE-2.0
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
-USA
-
-You can contact Health Market Science at info@healthmarketscience.com
-or at the following address:
-
-Health Market Science
-2700 Horizon Drive
-Suite 200
-King of Prussia, PA 19406
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 package com.healthmarketscience.sqlbuilder;
